db8ffc7f33dda1901fe4c9b4c20444a2e00b534f
leejwian <leejwian@gmail.com>
2025-03-28 15:28:54 +0900

    commit first

--- Baek_Python_Practice.py
+++ Baek_Python_Practice.py
@@ -1,19 +1,4 @@
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-
-numList = list(map(int, input().split()))
-
-sumList = [0]
-temp = 0
-
-for num in numList :
-    temp += num
-    
-    sumList.append(temp)
-
-for _ in range(M) :
-    i, j = map(int, input().split())
-
-    print(sumList[j] - sumList[i - 1])
+print("hello")
